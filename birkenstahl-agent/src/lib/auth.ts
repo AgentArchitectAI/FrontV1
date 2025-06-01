852a819af9675ad4e3ad43e09c3f1c7b1b82e879
Jorgearellano1 <129127690+Jorgearellano1@users.noreply.github.com>
2025-06-01 01:58:59 -0500

    feat:ready to connect to appwrite

--- birkenstahl-agent/src/lib/auth.ts
+++ birkenstahl-agent/src/lib/auth.ts
@@ -1,23 +1,16 @@
 import { account } from "./appwrite";
 import type { OAuthProvider } from "appwrite";
 
-const SUCCESS_REDIRECT = "/dashboard";
-const FAILURE_REDIRECT = "/login";
+const baseURL = window.location.origin; 
+const SUCCESS_REDIRECT = `${baseURL}/dashboard`;
+const FAILURE_REDIRECT = `${baseURL}/login`;
 
 export const loginWithGoogle = () => {
-  account.createOAuth2Session(
-    "google" as OAuthProvider,
-    SUCCESS_REDIRECT,
-    FAILURE_REDIRECT
-  );
+  account.createOAuth2Session("google" as OAuthProvider, SUCCESS_REDIRECT, FAILURE_REDIRECT);
 };
 
 export const loginWithGitHub = () => {
-  account.createOAuth2Session(
-    "github" as OAuthProvider,
-    SUCCESS_REDIRECT,
-    FAILURE_REDIRECT
-  );
+  account.createOAuth2Session("github" as OAuthProvider, SUCCESS_REDIRECT, FAILURE_REDIRECT);
 };
 
 export const logout = () => {
